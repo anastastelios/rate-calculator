f0969e7c10408876a9728af64294f4c6d352077d
Stelios Anastas <anastas.stelios@gmail.com>
2020-05-25 14:49:01 +0300

    Version 0.2.1

--- src/components/OperatorsPanel/index.js
+++ src/components/OperatorsPanel/index.js
@@ -9,7 +9,7 @@ const OperatorsPanel = (props) => {
         <div className='operators_panel_container'>
             <InvertSign handleCalculation={props.changeCalculation} curNumberSetter={props.curNumberSetter} prevNumberSetter={props.prevNumberSetter} lastBtnGetter={props.lastBtnGetter} />
             <OperatorBtn handleCalculation={props.changeCalculation} curNumberSetter={props.curNumberSetter} prevNumberSetter={props.prevNumberSetter} operatorSetter={props.operatorSetter} lastBtnSetter={props.lastBtnSetter} lastBtnGetter={props.lastBtnGetter} cancel='true' operator='AC' />
-            {arrayOfOperators.map(operator => <OperatorBtn handleCalculation={props.changeCalculation} prevNumberSetter={props.prevNumberSetter} prevNumberGetter={props.prevNumberGetter} operatorSetter={props.operatorSetter} operatorGetter={props.operatorGetter} curNumberSetter={props.curNumberSetter} curNumberGetter={props.curNumberGetter} lastBtnSetter={props.lastBtnSetter} lastBtnGetter={props.lastBtnGetter} operator={operator} />)}
+            {arrayOfOperators.map((operator, index) => <OperatorBtn key={index} handleCalculation={props.changeCalculation} prevNumberSetter={props.prevNumberSetter} prevNumberGetter={props.prevNumberGetter} operatorSetter={props.operatorSetter} operatorGetter={props.operatorGetter} curNumberSetter={props.curNumberSetter} curNumberGetter={props.curNumberGetter} lastBtnSetter={props.lastBtnSetter} lastBtnGetter={props.lastBtnGetter} operator={operator} />)}
         </div>
     )
 }
