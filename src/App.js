ff9928cb266c5134c821248b2958d9f2ce052821
Stelios Anastas <anastas.stelios@gmail.com>
2020-05-26 22:58:47 +0300

    Version 0.8.2 Added Keyboard Input, for Numpad also

--- src/App.js
+++ src/App.js
@@ -52,24 +52,25 @@ function App() {
   }
 
   const updateCalcFromKey = (e) => {
+    console.log(e.keyCode);
     e.persist();
     setCalculation(prevState => {
-      if (e.keyCode === 187 && e.shiftKey) {
+      if (e.keyCode === 187 && e.shiftKey || e.keyCode === 107) {
         handleOperator('+')
         setLastOperator('+')
         setLastBtn('O');
         return prevState
-      } else if (e.keyCode === 189) {
+      } else if (e.keyCode === 189 || e.keyCode === 109) {
         handleOperator('-')
         setLastOperator('-')
         setLastBtn('O');
         return prevState
-      } else if (e.keyCode === 56 && e.shiftKey) {
+      } else if (e.keyCode === 56 && e.shiftKey || e.keyCode === 106) {
         handleOperator('x')
         setLastOperator('x')
         setLastBtn('O');
         return prevState
-      } else if (e.keyCode === 191) {
+      } else if (e.keyCode === 191 || e.keyCode === 111) {
         handleOperator('/')
         setLastOperator('/')
         setLastBtn('O');
@@ -103,13 +104,13 @@ function App() {
           })
         }
       } else if (prevState.split('').length < 16) {
-        if (e.keyCode === 190) {
+        if (e.keyCode === 190 || e.keyCode === 110) {
           if (prevState.indexOf('.') < 0) {
             return prevState + '.';
           } else {
             return prevState;
           }
-        } else if (e.keyCode > 46 && e.keyCode < 58 && !e.shiftKey) {
+        } else if (e.keyCode > 47 && e.keyCode < 58 && !e.shiftKey) {
           if (prevState === '0' || lastBtn === 'O') {
             setLastBtn('N');
             return (e.keyCode - 48).toString()
@@ -117,15 +118,32 @@ function App() {
             setLastBtn('N');
             return prevState + (e.keyCode - 48).toString()
           }
+        } else if (e.keyCode > 95 && e.keyCode < 106) {
+          if (prevState === '0' || lastBtn === 'O') {
+            setLastBtn('N');
+            return (e.keyCode - 96).toString()
+          } else {
+            setLastBtn('N');
+            return prevState + (e.keyCode - 96).toString()
+          }
         } else {
           return prevState
         }
       } else {
-        if (e.keyCode > 46 && e.keyCode < 58 && !e.shiftKey) {
+        if (e.keyCode > 47 && e.keyCode < 58 && !e.shiftKey) {
           if (lastBtn === 'O') {
             setCalculation((e.keyCode - 48).toString());
             setLastBtn('N');
             return (e.keyCode - 48).toString()
+          } else {
+            setLastBtn('N');
+            return prevState
+          }
+        } else if (e.keyCode > 95 && e.keyCode < 106) {
+          if (lastBtn === 'O') {
+            setCalculation((e.keyCode - 96).toString());
+            setLastBtn('N');
+            return (e.keyCode - 96).toString()
           } else {
             setLastBtn('N');
             return prevState
